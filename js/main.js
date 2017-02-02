781a7c3a587549f15d93d336e0b78239ad832e2f
Sanket Mishra <sanket14mishra@gmail.com>
2017-02-02 21:52:40 +0530

    New Changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// $('document').load(function(){
+// 	$('#pre-load-wrap').removeClass("visible").addClass("hidden");
+// 	$('#content-wrap').removeClass("hidden").addClass("visible");
+// });
+
+
 $('document').ready(function(){
 
 	//Initialise Controller
@@ -29,10 +35,10 @@ $('document').ready(function(){
 	},"-=6");
 
 	var tl2 = new TimelineLite()
-	.from('#event', 1, {
+	.from('#event', 5, {
 		opacity:0
 	})	
-	.to('#event', 1, {
+	.to('#event', 5, {
 		opacity:1
 	})
 	.to('#event', 5, {
@@ -40,12 +46,37 @@ $('document').ready(function(){
 	},"+=2");
 
 	var tl3 = new TimelineLite()
-	.from('#abt-wrap', 2, {
+	.from('#abt-wrap', 5, {
 		opacity:0,
 	})
-	.to('#abt-wrap',2,{
+	.to('#abt-wrap',5,{
 		opacity: 1
-	});
+	})
+	.to('#abt-wrap', 5, {
+		opacity:0
+	},"+=2");
+
+	var tl4 = new TimelineLite()
+	.from('#gallery-wrap', 5, {
+		opacity:0,
+	})
+	.to('#gallery-wrap',5,{
+		opacity: 1
+	})
+	.to('#gallery-wrap', 5, {
+		opacity:0
+	},"+=2");
+
+	var tl5 = new TimelineLite()
+	.from('#team-wrap', 5, {
+		opacity:0,
+	})
+	.to('#team-wrap',5,{
+		opacity: 1
+	})
+	.to('#team-wrap', 5, {
+		opacity:0
+	},"+=2");
 
 
 	var gateZoomFade = new ScrollMagic.Scene({
@@ -55,8 +86,9 @@ $('document').ready(function(){
 		duration:"200%"
 	})
 	.setTween(tl1)
-	.addTo(controller);
-	
+	.addTo(controller)
+	.addIndicators();
+
 	var gatePin = new ScrollMagic.Scene({
 		triggerElement: '#svg-wrap',
 		triggerHook:0,
@@ -65,7 +97,8 @@ $('document').ready(function(){
 	})
 	.setPin("#svg-wrap")
 	.on("end",Start)
-	.addTo(controller);
+	.addTo(controller)
+	.addIndicators();
 
 	var eventFadeIn = new ScrollMagic.Scene({
 		triggerElement: '#event',
@@ -73,7 +106,8 @@ $('document').ready(function(){
 		duration: "100%"
 	})
 	.setTween(tl2)
-	.addTo(controller);
+	.addTo(controller)
+	.addIndicators();
 
 	var eventPin = new ScrollMagic.Scene({
 		triggerElement: '#event',
@@ -84,7 +118,8 @@ $('document').ready(function(){
 	.setPin("#event")
 	.on('start', Start1)
 	.on('end',Start)
-	.addTo(controller);
+	.addTo(controller)
+	.addIndicators();
 
 	var abtFadeIn = new ScrollMagic.Scene({
 		triggerElement: '#abt-wrap',
@@ -93,19 +128,93 @@ $('document').ready(function(){
 		duration: "100%"
 	})
 	.setTween(tl3)
-	.addTo(controller);
+	.addTo(controller)
+	.addIndicators();
 
 	var abtPin = new ScrollMagic.Scene({
 		triggerElement: '#abt-wrap',
 		triggerHook: 0,
 		offset: 0,
-		duration: "100%"
+		duration: "200%"
 	})
 	.setPin('#abt-wrap')
-	.on('start',Start1)
-	.addTo(controller);
+	.on('start', Start1)
+	.on('end',Start)
+	.addTo(controller)
+	.addIndicators();
 
-	
+	var galleryFadeIn = new ScrollMagic.Scene({
+		triggerElement: '#gallery-wrap',
+		triggerHook: 0,
+		offset: 0,
+		duration: "100%"
+	})
+	.setTween(tl4)
+	.addTo(controller)
+	.addIndicators();
+
+	var galleryPin = new ScrollMagic.Scene({
+		triggerElement: '#gallery-wrap',
+		triggerHook: 0,
+		offset: 0,
+		duration: "200%"
+	})
+	.setPin('#gallery-wrap')
+	.on('start', Start1)
+	.on('end',Start)
+	.addTo(controller)
+	.addIndicators();
+
+	var teamFadeIn = new ScrollMagic.Scene({
+		triggerElement: '#team-wrap',
+		triggerHook: 0,
+		offset: 0,
+		duration: "100%"
+	})
+	.setTween(tl5)
+	.addTo(controller)
+	.addIndicators();
+
+	var teamPin = new ScrollMagic.Scene({
+		triggerElement: '#team-wrap',
+		triggerHook: 0,
+		offset: 0,
+		duration: "200%"
+	})
+	.setPin('#team-wrap')
+	.on('start', Start1)
+	.on('end',Start)
+	.addTo(controller)
+	.addIndicators();
+
+	/*************** About Page ***************/
+	var countDownDate = new Date("Feb 17, 2017 00:00:00").getTime();
+
+
+var countdownfunction = setInterval(function() {
+
+    var now = new Date().getTime();
+    
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // countdown over
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("demo").innerHTML = "Spring has arrived!";
+	document.getElementById("demo1").innerHTML = "WATCH OUT";
+    }
+}, 1000);
+
+	/*************** Event Page ***************/
 
 	var isFull,thisPanel,thisItem,selectedItem,selectedDescription;
 
@@ -160,13 +269,13 @@ $('document').ready(function(){
 	});
 
 	function Start(){
-		var eventTop = ($(window).scrollTop()+winHeight/1.5);
+		var eventTop = ($(window).scrollTop()+winHeight-100);
 		var direction = controller.info("scrollDirection");
 		if(direction=="FORWARD"){
 		$(window).scrollTop(eventTop)}
 	}
 	function Start1(){
-		var eventTop = ($(window).scrollTop()-winHeight/1.5);
+		var eventTop = ($(window).scrollTop()-winHeight+100);
 		var direction = controller.info("scrollDirection");
 		if(direction=="REVERSE"){
 		$(window).scrollTop(eventTop);}
