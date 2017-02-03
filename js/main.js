aff6c9e74f00d2ac573c5c4051d5746d246df7c2
Sanket Mishra <sanket14mishra@gmail.com>
2017-02-03 22:07:56 +0530

    new updates

--- js/main.js
+++ js/main.js
@@ -10,11 +10,17 @@ $('document').ready(function(){
 	var controller = new ScrollMagic.Controller();
 	var winHeight= $(window).height();
 		console.log(winHeight);
-	
+	var winWidth = $(window).width();
+		console.log(winWidth);
+
+	var hasEventPageArrived = false;
+	console.log("declaring:"+hasEventPageArrived);
 
 	$(window).on("resize",function(){
 		winHeight = $(window).height();
 		console.log(winHeight);
+	    winWidth = $(window).width();
+		console.log(winWidth);
 	});
 
 
@@ -29,7 +35,7 @@ $('document').ready(function(){
 		marginTop: "-40%",
 		useFrames:true
 	})
-	.to('#svg-wrap',2,{
+	.to('#svg-wrap',5,{
 		opacity:0
 		
 	},"-=6");
@@ -68,22 +74,22 @@ $('document').ready(function(){
 	},"+=2");
 
 	var tl5 = new TimelineLite()
-	.from('#team-wrap', 5, {
+	.from('#team-wrap', 1, {
 		opacity:0,
-	})
-	.to('#team-wrap',5,{
-		opacity: 1
-	})
-	.to('#team-wrap', 5, {
-		opacity:0
-	},"+=2");
+	});
+	// .to('#team-wrap',5,{
+	// 	opacity: 1
+	// });
+	// .to('#team-wrap', 5, {
+	// 	opacity:0
+	// },"+=2");
 
 
 	var gateZoomFade = new ScrollMagic.Scene({
 		triggerElement: '#svg-wrap',
 		triggerHook: 0,
 		offset: 10,
-		duration:"200%"
+		duration:"100%"
 	})
 	.setTween(tl1)
 	.addTo(controller)
@@ -95,7 +101,7 @@ $('document').ready(function(){
 		offset: 0,
 		duration: "100%"
 	})
-	.setPin("#svg-wrap")
+	.setPin("#svg-wrap",{pushFollowers:false})
 	.on("end",Start)
 	.addTo(controller)
 	.addIndicators();
@@ -103,9 +109,12 @@ $('document').ready(function(){
 	var eventFadeIn = new ScrollMagic.Scene({
 		triggerElement: '#event',
 		triggerHook: 0,
+		offset:50,
 		duration: "100%"
 	})
 	.setTween(tl2)
+	.on('start',Start2)
+	.on('end',Start3)
 	.addTo(controller)
 	.addIndicators();
 
@@ -115,7 +124,7 @@ $('document').ready(function(){
 		offset: 0,
 		duration: "100%"
 	})
-	.setPin("#event")
+	.setPin("#event",{pushFollowers:false})
 	.on('start', Start1)
 	.on('end',Start)
 	.addTo(controller)
@@ -124,7 +133,7 @@ $('document').ready(function(){
 	var abtFadeIn = new ScrollMagic.Scene({
 		triggerElement: '#abt-wrap',
 		triggerHook: 0,
-		offset: 0,
+		offset: 50,
 		duration: "100%"
 	})
 	.setTween(tl3)
@@ -135,9 +144,9 @@ $('document').ready(function(){
 		triggerElement: '#abt-wrap',
 		triggerHook: 0,
 		offset: 0,
-		duration: "200%"
+		duration: "100%"
 	})
-	.setPin('#abt-wrap')
+	.setPin('#abt-wrap',{pushFollowers:false})
 	.on('start', Start1)
 	.on('end',Start)
 	.addTo(controller)
@@ -146,7 +155,7 @@ $('document').ready(function(){
 	var galleryFadeIn = new ScrollMagic.Scene({
 		triggerElement: '#gallery-wrap',
 		triggerHook: 0,
-		offset: 0,
+		offset: 50,
 		duration: "100%"
 	})
 	.setTween(tl4)
@@ -157,32 +166,32 @@ $('document').ready(function(){
 		triggerElement: '#gallery-wrap',
 		triggerHook: 0,
 		offset: 0,
-		duration: "200%"
+		duration: "100%"
 	})
-	.setPin('#gallery-wrap')
+	.setPin('#gallery-wrap',{pushFollowers:false})
 	.on('start', Start1)
 	.on('end',Start)
 	.addTo(controller)
 	.addIndicators();
 
-	var teamFadeIn = new ScrollMagic.Scene({
-		triggerElement: '#team-wrap',
-		triggerHook: 0,
-		offset: 0,
-		duration: "100%"
-	})
-	.setTween(tl5)
-	.addTo(controller)
-	.addIndicators();
+	// var teamFadeIn = new ScrollMagic.Scene({
+	// 	triggerElement: '#team-wrap',
+	// 	triggerHook: 0,
+	// 	offset: 0,
+	// 	duration: "100%"
+	// })
+	// .setTween(tl5)
+	// .addTo(controller)
+	// .addIndicators();
 
 	var teamPin = new ScrollMagic.Scene({
 		triggerElement: '#team-wrap',
 		triggerHook: 0,
 		offset: 0,
-		duration: "200%"
+		duration: "100%"
 	})
-	.setPin('#team-wrap')
-	.on('start', Start1)
+	.setPin('#team-wrap',{pushFollowers:false})
+	.on('start', fadeInLast)
 	.on('end',Start)
 	.addTo(controller)
 	.addIndicators();
@@ -203,8 +212,8 @@ var countdownfunction = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    // + minutes + "m " + seconds + "s ";
     
     // countdown over
     if (distance < 0) {
@@ -222,62 +231,186 @@ var countdownfunction = setInterval(function() {
 	thisItem='div.event.full div.event-list li.item1';
 	selectedDescription='div.event.full div.event-description div.item1';
 
+	$(window).on('scroll',function(){
+		if(!hasEventPageArrived){
+		$('div.event.panel')
+			.removeClass("cursor-pointer");
+		$('div.event-wrapper')
+			.removeClass("z-index");	
+			console.log("no");
+	}
+	else{
+		$('div.event.panel')
+			.addClass("cursor-pointer");
+		$('div.event-wrapper')
+			.addClass("z-index");	
+			console.log("yes");
+	}
+	});
+	
+
 	$('div.event.panel').click(function() {
-		if(!isFull){
-			t=false;
-			thisPanel=$(this);
-			thisPanel.addClass("full z-index");
-			$('body').addClass("hide-overflow");
-			thisPanel.one("transitionend", function() {
-				$('div.event.panel.full div.event-content-wrapper.hidden')
-					.addClass("visible");
-				});
-
-			$(selectedDescription).removeClass("visible");
-			$(thisItem).removeClass("selected");
-			thisItem='div.event.full div.event-list li.item1';
-			selectedDescription='div.event.full div.event-description div.item1';
-			$(thisItem).addClass("selected");
-			$(selectedDescription).addClass("visible");
-
-			isFull=true;
-		} 
-		$('div.event.panel.full div.close-button').on("click",function() {
-			thisPanel.removeClass("full");
-			$('body').removeClass("hide-overflow");
-			$('div.event-content-wrapper.hidden')
-			.removeClass("visible");
-			thisPanel.one("transitionend", function() {
-				
-				thisPanel.removeClass("z-index");
-				isFull=false;
-			});
+		console.log("in click");
+		if (hasEventPageArrived) {
 			
-		});
+			if(!isFull){
+				t=false;
+				thisPanel=$(this);
+				thisPanel.addClass("full z-index");
+				$('body').addClass("hide-overflow");
+				thisPanel.one("transitionend", function() {
+					$('div.event.panel.full div.event-content-wrapper.hidden')
+						.addClass("visible");
 
+					if(winWidth<767)
+					{
+						$('div.event.panel.full div.event-content-wrapper div.event-list')
+							.addClass("mobile-screen");
+						$('div.event.panel.full div.event-content-wrapper div.event-description')
+							.addClass("mobile-screen hidden");	
+					}
+					else{
+						$('div.event.panel.full div.event-content-wrapper div.event-list')
+							.removeClass("mobile-screen");
+					}
+					});
 
-		$('div.event.full div.event-list li').on("click",function() {
 				$(selectedDescription).removeClass("visible");
 				$(thisItem).removeClass("selected");
-				thisItem=$(this);
-				selectedItem=thisItem.attr("class");
-				console.log("clicked on "+selectedItem);
-				thisItem.addClass("selected");
-				selectedDescription='div.event.full div.event-description div.'+selectedItem;
-				$(selectedDescription).addClass("visible");
+				thisItem='div.event.full div.event-list li.item1';
+				selectedDescription='div.event.full div.event-description div.item1';
+				
+				
+					
+					
+					if(winWidth>767){
+						$(thisItem).addClass("selected");
+						$(selectedDescription).addClass("visible");
+					}
+
+				isFull=true;
+			} 
+			$('div.event.panel.full div.close-button').on("click",function() {
+				thisPanel.removeClass("full");
+				$('body').removeClass("hide-overflow");
+				$('div.event-list')
+					.removeClass("mobile-screen hidden");
+				$('div.event-description')
+					.removeClass("mobile-screen hidden");
+				$('div.event.panel div.back-button')
+					.addClass("hidden");	
+				$('div.event-content-wrapper.hidden')
+					.removeClass("visible");
+				thisPanel.one("transitionend", function() {
+					
+					thisPanel.removeClass("z-index",function(){
+							console.log("unhide");
+						});
+
+					isFull=false;
+				});
+				
 			});
-	});
+
+			$('div.event.panel.full div.back-button').on("click",function() {
+				
+				$('div.event.panel.full div.event-content-wrapper div.event-list')
+					.removeClass("hidden");
+				$('div.event.panel.full div.event-content-wrapper div.event-description')
+					.addClass("hidden");
+				$(this).addClass("hidden");	
+				$(selectedDescription)
+					.removeClass("visible");
+				$(thisItem)
+					.removeClass("selected");
+				
+			});
+
+
+			$('div.event.full div.event-list li').on("click",function() {
+					$(selectedDescription)
+						.removeClass("visible");
+					$(thisItem)
+						.removeClass("selected");
+					thisItem=$(this);
+					selectedItem=thisItem.attr("class");
+					thisItem.addClass("selected");
+					selectedDescription='div.event.full div.event-description div.'+selectedItem;
+					
+					if (winWidth>767) {
+						$(selectedDescription)
+							.addClass("visible");
+					}
+					else{
+						$('div.event.panel.full div.back-button')
+							.removeClass("hidden");
+						$('div.event.panel.full div.event-content-wrapper div.event-list')
+							.addClass("hidden");
+						$('div.event.panel.full div.event-content-wrapper div.event-description')
+							.removeClass("hidden");	
+						$(selectedDescription)
+							.addClass("visible");	
+					}
+				});
+	}});
 
 	function Start(){
-		var eventTop = ($(window).scrollTop()+winHeight-100);
+		// var eventTop = ($(window).scrollTop()+winHeight-100);
+		// var direction = controller.info("scrollDirection");
+		// if(direction=="FORWARD"){
+		// $(window).scrollTop(eventTop)}
+	}
+	function Start2(){
+		
 		var direction = controller.info("scrollDirection");
 		if(direction=="FORWARD"){
-		$(window).scrollTop(eventTop)}
+			hasEventPageArrived = true;
+		console.log("in function"+hasEventPageArrived);
+		}
+		else if(direction=="REVERSE"){
+			hasEventPageArrived = false;
+
+		console.log("back in function"+hasEventPageArrived);
+		}
 	}
-	function Start1(){
-		var eventTop = ($(window).scrollTop()-winHeight+100);
+
+	function Start3(){
 		var direction = controller.info("scrollDirection");
 		if(direction=="REVERSE"){
-		$(window).scrollTop(eventTop);}
+			hasEventPageArrived = true;
+		console.log("in function"+hasEventPageArrived);
+		}
+		else if(direction=="FORWARD"){
+			hasEventPageArrived = false;
+
+		console.log("back in function"+hasEventPageArrived);
+		}
 	}
+
+	function Start1(){}
+
+	function fadeInLast(){
+		$('.team-wrap').css("overflow","auto");
+		var direction = controller.info("scrollDirection");
+		if(direction=="FORWARD"){
+		var tl6 = new TimelineLite()
+	.to('#team-wrap',5,{
+		opacity:1
+	});}
+	else if(direction=="REVERSE"){
+		$('.team-wrap').css("overflow","hidden");
+		var tl6 = new TimelineLite()
+	.to('#team-wrap',0.1,{
+		opacity:0
+	});
+	}
+	}
+
+	// function fadeOutLast(){
+	// 	$('.team-wrap').css("overflow","hidden");
+	// 	var tl6 = new TimelineLite()
+	// .to('#team-wrap',5,{
+	// 	opacity:0
+	// });
+	// }
 });
